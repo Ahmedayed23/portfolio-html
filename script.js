674d2f9839851a0a8256544bd11e5b9b1dbdd5da
Ahmedayed23 <ahmed28ayed@gmail.com>
2024-04-07 11:09:03 +0200

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+const nav= document.querySelector("nav");
+console.log(nav);
+const hobbiesE1=document.querySelector(".hobbies-element");
+console.log(hobbiesE1);
+const about=document.querySelector("#about");
+console.log(about);
+const li =document.querySelector("li");
+console.log(li);
